972f7ad3f3d4f04a3a3d7f4ebe064a11fd4d0b58
amansingh85589 <singhaman85589@gmail.com>
2025-03-05 17:46:48 +0530

    Your commit message

--- src/routes/user.js
+++ src/routes/user.js
@@ -1,7 +1,11 @@
 
 
 import { Router } from "express";
-import { loginUser, registerUser, logOutUser, refreshAccessToken } from "../controller/user.js";
+
+import { loginUser, registerUser, logOutUser, refreshAccessToken, 
+    changeCurrentPassword, getCurrentUser, updateAccountDetails,
+     updateUserAvatar, updateUserCoverImage, getUserChannelProfile,
+      getWatchHistory } from "../controller/user.js";
 
 // import {loginUser, registerUser} from "../controller/user.js";
 import { upload } from "../middlewares/multer.middleware.js";
@@ -28,5 +32,24 @@ router.route("/login").post(loginUser)
 
 router.route("/logout").post(verifyJWT, logOutUser)
 router.route("/refresh-token").post(refreshAccessToken)
+
+
+router.route("/change-password").post(verifyJWT,changeCurrentPassword)
+
+router.route("/current-user").get(verifyJWT,getCurrentUser)
+
+router.route("/update-account").patch(verifyJWT, updateAccountDetails)
+
+
+router.route("/avatar").patch(verifyJWT, upload.single("avatar"), updateUserAvatar)
+
+Router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"), updateUserCoverImage)
+
+
+Router.route("/c/:username").get(verifyJWT, getUserChannelProfile)
+
+
+router.route("/history").get(verifyJWT,getWatchHistory)
+
 
 export default router;
